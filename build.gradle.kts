be671d84cceccf3655cd1ba3259b18dd0df4cd45
Boomaa23 <cmo93003@yahoo.com>
2019-10-05 21:43:40 -0700

    Remove excess dependencies and fix Travis

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,6 @@ import com.techshroom.inciseblue.commonLib
 
 plugins {
     id("org.rivierarobotics.gradlerioredux") version "0.6.0"
-    id("net.ltgt.apt-idea") version "0.21"
-    id("net.ltgt.apt-eclipse") version "0.21"
 }
 
 gradleRioRedux {
@@ -12,14 +10,6 @@ gradleRioRedux {
 }
 
 dependencies {
-	commonLib("net.octyl.apt-creator", "apt-creator", "0.1.4") {
-		compileOnly(lib("annotations"))
-		annotationProcessor(lib("processor"))
-	}
-	commonLib("com.google.dagger", "dagger", "2.21") {
-		implementation(lib())
-		annotationProcessor(lib("compiler"))
-	}
 	implementation("org.slf4j:slf4j-api:1.7.25")
 	commonLib("ch.qos.logback", "logback", "1.2.3") {
 		implementation(lib("classic"))
